cc2f93de9e10cc085a687c0c75bc60ed870425f8
BassaniDavid <davbassan98@gmail.com>
2025-03-04 15:17:21 +0100

    feat: control if js works when click on the button

--- js/app.js
+++ js/app.js
@@ -1,2 +1,10 @@
 
 const lightOff = document.querySelector('.lamp-off')
+
+const buttonOff = document.querySelector('button')
+
+buttonOff.addEventListener(
+    'click', function() {
+        console.log('tasto cliccato')
+    }
+)
